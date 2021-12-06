3d6396c67aa29e18e2fb11804e5e442223078815
riebelc <riebelc@lancastercountryday.org>
2021-12-06 13:02:23 -0500

    gyro for myself

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRun.java
@@ -1,7 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
-public class MainRun {
-    public void something(){
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class MainRun extends OpMode {
+    BNO055IMU imu;
+    public void init() {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+
+    public void loop(){
+        telemetry.addLine("Angle: " + imu.getAngularOrientation());
     }
 }
